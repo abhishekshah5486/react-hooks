2cf7fae348f4ae893aae6e6b13c154bbf28fd80b
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-06-08 22:31:09 +0530

    useRef and useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [inputV, setInputV] = useState('');
+  let pRef = useRef(0);
+  let iRef = useRef(0);
+
+  function displayOutput(){
+    // let input = iRef.current.value;
+    // let string = "Over here the output would appear - ";
+    // pRef.current.innerText = string + input;
+
+    setInputV(iRef.current.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input ref={iRef} onChange={displayOutput} type='text'></input>
+        <p ref={pRef}>Over here the output would appear - {inputV}</p>
     </div>
   );
 }
 
 export default App;
+
+
+// Whenever a state variable is set,
+// it would cause a re-render of the component and the value of the variable would be retained on re-render
+// Mounting and Re-Render
+// Mounting - When a function/component is called for the first time
+// UseState accepts 2 parameters
+// StateVariable and SetStateVariable
+// Every time a stateVariable is set, it would cause a re-render of the component
